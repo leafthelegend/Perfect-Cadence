565a527522a661c28111f219ab4eb9ab581928ab
leafthelegend <liefcode@gmail.com>
2023-08-25 17:39:48 +1000

    Initial commit

---
+++ Perfect Cadence/Perfect Cadence/Perfect_CadenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Perfect_CadenceApp.swift
+//  Perfect Cadence
+//
+//  Created by Lief Lundmark-Aitcheson on 25/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct Perfect_CadenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
