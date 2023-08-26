c864a2ac5be4443f3db688d888278b2020357aa7
Ei3-kw <52182320+Ei3-kw@users.noreply.github.com>
2023-08-26 17:34:05 +1000

    Revert "Revert "Merge branch 'main' of https://github.com/leafthelegend/Perfect-Cadence""
    This reverts commit f07573a5bb3242b03d80e480c539f61053a7c01f.

---
+++ Perfect Cadence/Perfect Cadence/Pace tracking/PaceTrackingModel.swift
@@ -0,0 +1,30 @@
+//
+//  PaceTrackingModel.swift
+//  Perfect Cadence
+//
+//  Created by Ella WANG on 26/8/2023.
+//
+
+import Foundation
+import CoreMotion
+
+class PaceTrackingModel: ObservableObject {
+    private let pedometer = CMPedometer()
+    private let startTime = Date()
+    @Published var cadence: Float? = 0.0
+    
+    init() {
+        startUpdating()
+        print(CMPedometer.isCadenceAvailable())
+    }
+    
+    func startUpdating() {
+        pedometer.startUpdates(
+            from : Date(),
+            withHandler: { data, error in
+                print("\(data)")
+                self.cadence = data?.currentCadence?.floatValue
+            }
+        )
+    }
+}
